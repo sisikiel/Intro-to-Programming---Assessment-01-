3311ef0be4e85189856f116f26605638ec745ae5
sisikiel <sisibytheseashore@gmail.com>
2024-11-07 14:31:08 +0400

    Add files via upload

---
+++ Exercise 01 - Coding is Cool.py
@@ -0,0 +1,9 @@
+#I am tasked to print out the following phrase: "Coding is Cool" while using variables and string concentration.
+    #Variables are the places where you can store your data or value.
+
+#variable = value
+word1 = "Coding "
+word2 = "is "
+word3 = "Cool!!"
+print(word1 + word2 + word3)
+    # for printing, use " + " symbol to add the words together.
