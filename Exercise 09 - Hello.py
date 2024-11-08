e9d9fa09431d06425b170eebf5c3f24c54762cd5
sisikiel <sisibytheseashore@gmail.com>
2024-11-08 14:35:06 +0400

    Exercise 09 - Hello.py

--- Exercise 09 - Hello.py
+++ Exercise 09 - Hello.py
@@ -1,11 +1,13 @@
 #fill in the blanks
 
+#create a function that stores "hello"
 def hello():
     print("hello")
 
-
+#create the main function that stores the previous function.
 def main():
     hello()
 
+#call out the main function to display the word "hello"
 if __name__ == "__main__": 
     main()
